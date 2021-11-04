88981017b11c9632425804f5b8ad6227301ede43
MetteMy <mettemy@gabelgaard.org>
2021-11-04 21:05:39 +0100

    fixed player moving

--- Board.js
+++ Board.js
@@ -1,52 +1,48 @@
-let x;
 let y;
-let a;
-let b;
-let num;
-let k;
-let q1;
-let q2;
-let q3;
-
+let dieMoves;
+let player1 = 0;
+let squareX = [];
+let colors = ["red", "yellow", "green", "purple"];
+let color;
+let moves = 0;
 function setup() {
-  createCanvas(600, 600);
-  background(220);
-
- x = 5;
- y = height/2;
- a = 50;
- b = 50;
- num = 33;
- noStroke();
- q1 = (239,239,0);//gul
- q2 = (215,1,23);//rød
- q3 = (100,166,23);//grøn
-
- //Øverste række
-for (let i = 1; i < num/3; i++){
- fill(239,239,0);
- //fill(239,239,0);
- rect(x,y,a,b);
- x += 60;}
-
-
-//Terning
-button = createButton('Terning');
-button.position(10,10);
-button.size(70,70);
-button.mousePressed(terning);
+    createCanvas(windowWidth, windowHeight);
+    background(220);
+    noStroke();
+    rectMode(CENTER);
+    y = height / 2;
+    drawBoard();
+    //Terning
+    button = createButton('Terning');
+    button.position(10, 10);
+    button.size(70, 70);
+    button.mousePressed(die);
 }
 
-function terning(){
-  button = createButton(random(1,6));
-  button.position(20,20);
-  button.size(5,5);
+function die() {
+    push();
+    fill(220);
+    rect(90, 50, 20, 20);
+    pop();
+    
+    dieMoves = parseInt(random(1, 7));
+    text(dieMoves, 90, 50);
+    moves += dieMoves;
+    player1 = squareX[moves];
+    drawBoard();
+    circle(player1, y, 30, 30);
+
 }
-
-
-
-
-
+function drawBoard() {
+    for (let i = 0; i < 20; i++) {
+        color = i % 4;
+        push();
+        fill(colors[color]);
+        rect(width / 20 * i + 32, y, 50, 50);
+        squareX.push(width / 20 * i + 32);
+        pop();
+    }
+}
 
 function draw() {
 
