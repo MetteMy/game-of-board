df7b4c897ec5b15236c163c65fc1ce915f307544
MetteMy <mettemy@gabelgaard.org>
2021-11-04 21:20:17 +0100

    Added Amelies questions

--- Questions.js
+++ Questions.js
@@ -1,5 +1,5 @@
-var eGeographyQ = ['fujire',"wjæ","djfios","wdjio"];
-var eGeographyA = [];
+var eGeographyQ = ["What is the capital of Sweden?", "What is the capital of Norway?", "Which country is the biggest in the world?", "Copenhagen is the capital of…?", "What is the capital of Germany?", "Paris is the capital of…?", "Brussels is the capital of…?", "Athens is the capital of…?", "Which country is the best country?", "Which country is the most populated?"];
+var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France", "Belgium", "Greece", "Denmark", "China"];
 
 var mGeographyQ = [];
 var mGeographyA = [];
