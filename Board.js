2df06a0092bfad7c647d8806114b11dcb4d6fa21
MetteMy <mettemy@gabelgaard.org>
2021-11-11 14:56:53 +0100

    fixed categories to colors

--- Board.js
+++ Board.js
@@ -5,19 +5,29 @@ let squareX = [];
 let colors = ["red", "yellow", "green", "purple"];
 let color;
 let moves = 0;
+let gameOver;
+let category = [];
+let answer = [];
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(220);
     noStroke();
     rectMode(CENTER);
     textAlign(CENTER);
+    gameOver = false;
     y = height / 2;
     drawBoard();
     //Dice
     button = createButton('Dice');
+    answerBtn = createButton('Answer');
     button.position(10, 10);
     button.size(70, 70);
     button.mousePressed(die);
+    eGQ = eGeographyQ;
+    eGA = eGeographyA;
+    eMQ = eMathsQ;
+    eMA = eMathsA;
 }
 
 
@@ -33,28 +43,22 @@ function die() {
     player1 = squareX[moves];
     drawBoard();
     circle(player1, y, 30, 30);
+    giveQuestion()
     if (moves >= 20){
         console.log("you won!!!");
-        background(220)
+        background(220);
         text("The Game is over. Player1 is the winner!", width/2, height/2);
-
+        button.hide();
+        answerBtn.hide();
+        gameOver = true;
+        
     }
-    giveQuestion()
-    rect(width/2,120, 400,200);
-    let question = parseInt(random(0, eGeographyQ.length));
-    console.log(question);
     
-    text(eGeographyQ[question],width/2,120);
-    answerBtn = createButton('Answer');
-    answerBtn.position(width/2, 140);
-    answerBtn.mousePressed(function() {text(eGeographyA[question], width/2, 180)});
-    //Husk at fjerne spørgsmål når de er brugt 
+    
         
     
 }
-function getAnswer() {
-    text(eGeographyA[question], width/2, 180);
-}
+
 
 
 
@@ -73,8 +77,49 @@ function drawBoard() {
 
 
 function giveQuestion() {
+    if (gameOver === false){
+    rect(width/2,120, 400,200);
     
+    console.log(moves);
+    if (moves %4 === 0){
+    // math 
+    category = eMQ;
+    answer = eMA;
+    }
+    if (moves %4 === 1){
+    // random
+    category = ["randomQ"];
+    answer = ["randomA"];
+    }
+    if (moves % 4 === 2){
+    // geo
+    category = eGQ;
+    answer = eGA;
+    }
+    if (moves %4 === 3){
+    // popculture
+    category = ["popcultureQ"];
+    answer = ["popcultureA"];
+       }
+    let question = parseInt(random(0, category.length));
+    console.log(question);
+    text(category[question],width/2,120);
+    answerBtn.show();
+    answerBtn.position(width/2 - 30, 140);
+    answerBtn.mousePressed(function() {text(answer[question], width/2, 180);
+    category.splice(question, 1);
+    answer.splice(question, 1);
+    answerBtn.hide();
+    console.log(category);
+    console.log(answer);
+    //eGeographyA.remove(question);
+    });
+    //Husk at fjerne spørgsmål når de er brugt 
+    
+    }
 }
+
+
 function draw() {
 
 }
--- Questions.js
+++ Questions.js
@@ -1,3 +1,5 @@
+let eGQ = [];
+let eGA = [];
 var eGeographyQ = ["What is the capital of Sweden?", "What is the capital of Norway?", "Which country is the biggest in the world?", "Copenhagen is the capital of…?", "What is the capital of Germany?", "Paris is the capital of…?", "Brussels is the capital of…?", "Athens is the capital of…?", "Which country is the best country?", "Which country is the most populated?"];
 var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France", "Belgium", "Greece", "Denmark", "China"];
 
@@ -7,8 +9,10 @@ var mGeographyA = [];
 var hGeographyQ = [];
 var hGeographyA = [];
 
-var MathsQ = [];
-var MathsA = [];
+let eMQ = [];
+let eMA = [];
+var eMathsQ = ["1+2=","3+3=" ];
+var eMathsA = ["3", "6"];
 
 var popcultureQ = [];
 var popcultureA = [];
