e58a8978516be8e20a8cc716aefec3262f6add15
amel0232 <93131100+amel0232@users.noreply.github.com>
2021-11-24 09:17:01 +0100

    Svær Geo færdig

--- Questions.js
+++ Questions.js
@@ -6,8 +6,8 @@ var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France",
 var mGeographyQ = ["Name a country starting with a V", "Which country has the longest coastline?", "What is the capital of Finland?", "Which country has the Kuala Lumpur as the capital?", "What is the capital of Spain?", "Name a country ending with the letter L", "Name the country with K as the last letter", "Name a country with the last letter M", "The capital of Canada is…?", "The Capital of Egypt is…?"];
 var mGeographyA = ["Vanuatu, Venezuela or Vietnam", "Canada", "Helsinki", "Malaysia", "Madrid", "Nepal, Portugal, Senegal, Brazil or Israel", "Denmark", "Belgium, United Kingdom or Vietnam", "Ottawa", "Cairo"];
 
-var hGeographyQ = ["How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?"];
-var hGeographyA = ["195", "Russia (with 16)", "Canberra", "France, Austria, Turkey, Serbia, Tanzania or Zambia", "27", "Vatican City", "New Zealand", "Canada"];
+var hGeographyQ = ["How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?", "What country is the least populated in the world?", "Lomé is the capital of…?"];
+var hGeographyA = ["195", "Russia (with 16)", "Canberra", "France, Austria, Turkey, Serbia, Tanzania or Zambia", "27", "Vatican City", "New Zealand", "Canada", "Vatican City (about 800 people)", "Togo"];
 
 var eMathsQ = [];
 var eMathsA = [];
