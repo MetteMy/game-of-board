31cb30168c18525ffef33f898fdf0df834cb22a6
amel0232 <93131100+amel0232@users.noreply.github.com>
2021-11-24 08:41:03 +0100

    Spørgsmål godt

--- Questions.js
+++ Questions.js
@@ -1,17 +1,35 @@
 var eGeographyQ = ["What is the capital of Sweden?", "What is the capital of Norway?", "Which country is the biggest in the world?", "Copenhagen is the capital of…?", "What is the capital of Germany?", "Paris is the capital of…?", "Brussels is the capital of…?", "Athens is the capital of…?", "Which country is the best country?", "Which country is the most populated?"];
 var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France", "Belgium", "Greece", "Denmark", "China"];
 
-var mGeographyQ = [];
-var mGeographyA = [];
+var mGeographyQ = ["Name a country starting with a V", "Which country has the longest coastline?", "What is the capital of Finland?", "Which country has the Kuala Lumpur as the capital?", "What is the capital of Spain?", "Name a country ending with the letter L", "Name the country with K as the last letter", "Name a country with the last letter M", "The capital of Canada is…?", "The Capital of Egypt is…?"];
+var mGeographyA = ["Vanuatu, Venezuela or Vietnam", "Canada", "Helsinki", "Malaysia", "Madrid", "Nepal, Portugal, Senegal, Brazil or Israel", "Denmark", "Belgium, United Kingdom or Vietnam", "Ottawa", "Cairo"];
 
-var hGeographyQ = [];
-var hGeographyA = [];
+var hGeographyQ = ["How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?"];
+var hGeographyA = ["195", "Russia (with 16)", "Canberra", "France, Austria, Turkey, Serbia, Tanzania or Zambia", "27", "Vatican City", "New Zealand", "Canada"];
 
-var MathsQ = [];
-var MathsA = [];
+var eMathsQ = [];
+var eMathsA = [];
 
-var popcultureQ = [];
-var popcultureA = [];
+var mMathsQ = [];
+var mMathsA = [];
 
-var randomQ = [];
-var randomA = [];
+var hMathsQ = [];
+var hMathsA = [];
+
+var ePopcultureQ = [];
+var ePopcultureA = [];
+
+var mPopcultureQ = [];
+var mPopcultureA = [];
+
+var hPopcultureQ = [];
+var hPopcultureA = [];
+
+var eRandomQ = [];
+var eRandomA = [];
+
+var mRandomQ = [];
+var mRandomA = [];
+
+var hRandomQ = [];
+var hRandomA = [];
