0676abcbb60987492f2cdebc2e2097a595553e27
amel0232 <93131100+amel0232@users.noreply.github.com>
2021-12-01 08:55:32 +0100

    All questions in easy

--- Questions.js
+++ Questions.js
@@ -1,7 +1,7 @@
 let eGQ = [];
 let eGA = [];
-var eGeographyQ = ["What is the capital of Sweden?", "What is the capital of Norway?", "Which country is the biggest in the world?", "Copenhagen is the capital of…?", "What is the capital of Germany?", "Paris is the capital of…?", "Brussels is the capital of…?", "Athens is the capital of…?", "Which country is the best country?", "Which country is the most populated?"];
-var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France", "Belgium", "Greece", "Denmark", "China"];
+var eGeographyQ = ["What is the capital of Sweden?", "What is the capital of Norway?", "Which country is the biggest in the world?", "Copenhagen is the capital of…?", "What is the capital of Germany?", "Paris is the capital of…?", "Brussels is the capital of…?", "Athens is the capital of…?", "Which country is the best country?", "Which country is the most populated?", "Name a country starting with a V", "Which country has the longest coastline?", "What is the capital of Finland?", "Which country has the Kuala Lumpur as the capital?", "What is the capital of Spain?", "Name a country ending with the letter L", "Name the country with K as the last letter", "Name a country with the last letter M", "The capital of Canada is…?", "The Capital of Egypt is…?", "How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?", "What country is the least populated in the world?", "Lomé is the capital of…?"];
+var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France", "Belgium", "Greece", "Denmark", "China", "Vanuatu, Venezuela or Vietnam", "Canada", "Helsinki", "Malaysia", "Madrid", "Nepal, Portugal, Senegal, Brazil or Israel", "Denmark", "Belgium, United Kingdom or Vietnam", "Ottawa", "Cairo", "195", "Russia (with 16)", "Canberra", "France, Austria, Turkey, Serbia, Tanzania or Zambia", "27", "Vatican City", "New Zealand", "Canada", "Vatican City (about 800 people)", "Togo"];
 
 var mGeographyQ = ["Name a country starting with a V", "Which country has the longest coastline?", "What is the capital of Finland?", "Which country has the Kuala Lumpur as the capital?", "What is the capital of Spain?", "Name a country ending with the letter L", "Name the country with K as the last letter", "Name a country with the last letter M", "The capital of Canada is…?", "The Capital of Egypt is…?"];
 var mGeographyA = ["Vanuatu, Venezuela or Vietnam", "Canada", "Helsinki", "Malaysia", "Madrid", "Nepal, Portugal, Senegal, Brazil or Israel", "Denmark", "Belgium, United Kingdom or Vietnam", "Ottawa", "Cairo"];
@@ -12,26 +12,26 @@ var hGeographyA = ["195", "Russia (with 16)", "Canberra", "France, Austria, Turk
 var eMathsQ = ["2+5", "9-4", "2∙3", "4/2", "10∙2", "3∙4", "17+5", "23+8", "41-17", "32-9"];
 var eMathsA = ["7", "5", "6", "2", "20", "12", "22", "31", "24", "23"];
 
-var mMathsQ = [];
-var mMathsA = [];
+var mMathsQ = ["9∙19", "", "", "", "", "", "", "", "", ""];
+var mMathsA = ["117", "", "", "", "", "", "", "", "", ""];
 
-var hMathsQ = [];
-var hMathsA = [];
+var hMathsQ = ["144/32", "", "", "", "", "", "", "", "", ""];
+var hMathsA = ["4.5", "", "", "", "", "", "", "", "", ""];
 
-var ePopcultureQ = [];
-var ePopcultureA = [];
+var ePopcultureQ = ["", "", "", "", "", "", "", "", "", ""];
+var ePopcultureA = ["", "", "", "", "", "", "", "", "", ""];
 
-var mPopcultureQ = [];
-var mPopcultureA = [];
+var mPopcultureQ = ["", "", "", "", "", "", "", "", "", ""];
+var mPopcultureA = ["", "", "", "", "", "", "", "", "", ""];
 
-var hPopcultureQ = [];
-var hPopcultureA = [];
+var hPopcultureQ = ["", "", "", "", "", "", "", "", "", ""];
+var hPopcultureA = ["", "", "", "", "", "", "", "", "", ""];
 
-var eRandomQ = [];
-var eRandomA = [];
+var eRandomQ = ["", "", "", "", "", "", "", "", "", ""];
+var eRandomA = ["", "", "", "", "", "", "", "", "", ""];
 
-var mRandomQ = [];
-var mRandomA = [];
+var mRandomQ = ["", "", "", "", "", "", "", "", "", ""];
+var mRandomA = ["", "", "", "", "", "", "", "", "", ""];
 
-var hRandomQ = [];
-var hRandomA = [];
+var hRandomQ = ["", "", "", "", "", "", "", "", "", ""];
+var hRandomA = ["", "", "", "", "", "", "", "", "", ""];
