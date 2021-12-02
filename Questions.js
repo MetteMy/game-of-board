4dab44c5d51b16b382a2ed6dec2c197be0e64a27
amel0232 <93131100+amel0232@users.noreply.github.com>
2021-12-02 15:44:51 +0100

    Mangler stadig Maths & Random færdig

--- Questions.js
+++ Questions.js
@@ -1,14 +1,14 @@
 let eGQ = [];
 let eGA = [];
-//we made different difficulties, but we do not have enough time left
-//to implement them, so we will be using them all but have no way to choose difficulty
+//Vi har lavet spørgsmål med forskellig sværhedsgrad, men kan ikke nå at implementere en mulighed for at vælge
+//sværhedsgrad, så 
 var eGeographyQ = ["What is the capital of Sweden?", "What is the capital of Norway?", "Which country is the biggest in the world?", "Copenhagen is the capital of…?", "What is the capital of Germany?", "Paris is the capital of…?", "Brussels is the capital of…?", "Athens is the capital of…?", "Which country is the best country?", "Which country is the most populated?", "Name a country starting with a V", "Which country has the longest coastline?", "What is the capital of Finland?", "Which country has the Kuala Lumpur as the capital?", "What is the capital of Spain?", "Name a country ending with the letter L", "Name the country with K as the last letter", "Name a country with the last letter M", "The capital of Canada is…?", "The Capital of Egypt is…?", "How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?", "What country is the least populated in the world?", "Lomé is the capital of…?"];
 var eGeographyA = ["Stockholm", "Oslo", "Russia", "Denmark", "Berlin", "France", "Belgium", "Greece", "Denmark", "China", "Vanuatu, Venezuela or Vietnam", "Canada", "Helsinki", "Malaysia", "Madrid", "Nepal, Portugal, Senegal, Brazil or Israel", "Denmark", "Belgium, United Kingdom or Vietnam", "Ottawa", "Cairo", "195", "Russia (with 16)", "Canberra", "France, Austria, Turkey, Serbia, Tanzania or Zambia", "27", "Vatican City", "New Zealand", "Canada", "Vatican City (about 800 people)", "Togo"];
 
 var mGeographyQ = ["Name a country starting with a V", "Which country has the longest coastline?", "What is the capital of Finland?", "Which country has the Kuala Lumpur as the capital?", "What is the capital of Spain?", "Name a country ending with the letter L", "Name the country with K as the last letter", "Name a country with the last letter M", "The capital of Canada is…?", "The Capital of Egypt is…?"];
 var mGeographyA = ["Vanuatu, Venezuela or Vietnam", "Canada", "Helsinki", "Malaysia", "Madrid", "Nepal, Portugal, Senegal, Brazil or Israel", "Denmark", "Belgium, United Kingdom or Vietnam", "Ottawa", "Cairo"];
 
-var hGeographyQ = ["How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?", "What country is the least populated in the world?", "Lomé is the capital of…?"];
+var hGeographyQ = ["How many countries exist officially?", "Which country has the biggest number of neighbouring countries?", "Name the capital of Australia?", "Name a country with exactly 8 neighbouring countries", "How many countries in the EU?", "What country is the smallest in the world?", "Country furthest from Denmark?", "Name the country with the longest coastline?", "What country is the least populated in the world?", "Lomé is the capital of…?"];
 var hGeographyA = ["195", "Russia (with 16)", "Canberra", "France, Austria, Turkey, Serbia, Tanzania or Zambia", "27", "Vatican City", "New Zealand", "Canada", "Vatican City (about 800 people)", "Togo"];
 
 var eMathsQ = ["2+5", "9-4", "2∙3", "4/2", "10∙2", "3∙4", "17+5", "23+8", "41-17", "32-9"];
@@ -26,14 +26,14 @@ var ePopcultureA = ["No worries", "Avengers: Endgame", "Elon Musk", "Sweden", "E
 var mPopcultureQ = ["What is the name of Ariel and Prince Eric’s daughter?", "Which Disney Princess sings “Once Upon a Dream”?", "How many Star Wars movies are there (not including spin-offs like Rogue One: A Star Wars Story and Solo)?", "Who was the drummer for The Beatles?", "How many movies are there in the film series The Godfather?", "How many movies has Tom Hanks been in?", "How many ghosts are there in A Christmas Carol?", "What is Tim Burton’s 1993 famous Christmas movie?", "What company has used Santa Claus in its advertisements every year since 1931?", "Black Panther starred which famous actor?"];
 var mPopcultureA = ["Princess Melody", "Aurora (Sleeping Beauty)", "9", "Ringo Starr", "3", "More than 72", "Four (Christmas Past, Christmas Present, Christmas Future & Jacob Marley)", "The Nightmare Before Christmas", "Coca-Cola", "Chadwick Boseman"];
 
-var hPopcultureQ = ["What is the real name of Joe Exotic (Tiger King)?", "How many kids does Angelina Jolie have?", "What year did Disneyland open?", "What is the name of Wendy’s dog in Peter Pan?", "How many brothers does Prince Hans of the Southern Isles have in Frozen?", "What was the first music video played on MTV?", "What was Freddie Mercury’s real name?", "Which movie (adjusted for inflation) is the highest-grossing movie to date?", "What was the first Pixar movie", "What year was Britney Spears’ album Oops!… I Did It Again released?"];
+var hPopcultureQ = ["What is the real name of Joe Exotic (Tiger King)?", "How many kids does Angelina Jolie have?", "What year did Disneyland open?", "What is the name of Wendy’s dog in Peter Pan?", "How many brothers does Prince Hans of the Southern Isles have in Frozen?", "What was the first music video played on MTV?", "What was Freddie Mercury’s original name?", "Which movie (adjusted for inflation) is the highest-grossing movie to date?", "What was the first Pixar movie", "What year was Britney Spears’ album Oops!… I Did It Again released?"];
 var hPopcultureA = ["Joseph Allen Maldonado-Passage", "6", "1955", "Nana", "12", "Video Killed the Radio Star", "Frederick Bulsara", "Gone with the Wind", "Toy Story", "2000"];
 
-var eRandomQ = ["How many hours in year?", "", "", "", "", "", "", "", "", ""];
-var eRandomA = ["About 8765.81277 hours", "", "", "", "", "", "", "", "", ""];
+var eRandomQ = ["How many hours in a day?", "", "", "", "", "", "", "", "", ""];
+var eRandomA = ["24", "", "", "", "", "", "", "", "", ""];
 
-var mRandomQ = ["", "", "", "", "", "", "", "", "", ""];
-var mRandomA = ["", "", "", "", "", "", "", "", "", ""];
+var mRandomQ = ["How old is Mette Frederiksen?", "", "", "", "", "", "", "", "", ""];
+var mRandomA = ["44 years old", "", "", "", "", "", "", "", "", ""];
 
-var hRandomQ = ["", "", "", "", "", "", "", "", "", ""];
-var hRandomA = ["", "", "", "", "", "", "", "", "", ""];
+var hRandomQ = ["How many hours in year?", "", "", "", "", "", "", "", "", ""];
+var hRandomA = ["About 8765.81277 hours", "", "", "", "", "", "", "", "", ""];
