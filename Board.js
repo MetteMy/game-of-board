309ef5075d4f4adce4debdd67b0765c18e452504
MetteMy <mettemy@gabelgaard.org>
2021-12-17 10:10:59 +0100

    Formatting

--- Board.js
+++ Board.js
@@ -18,7 +18,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight);//Canvas tilpasses til computeren
     background("#1982C4");//blå
     strokeWeight(2);
-    
+
     rectMode(CENTER);//Firkanter tegnes fra midten
     textAlign(CENTER);//Test er centeret
     textSize(20);
@@ -31,7 +31,7 @@ function setup() {
     startGameBtn = createButton('startgame').style('background-color', "white");;//Opretter startknap: 'startgame'
     playerSelect = createSelect().style('background-color', "white");; //Opretter selcect bar
     //Indsætter valgmuligheder for antal players
-    
+
     playerSelect.option('2');
     playerSelect.option('3');
     playerSelect.option('4');
@@ -39,11 +39,11 @@ function setup() {
 
 
 
-    
+
     //players.push(new Player("Pink", "pink", height / 2 + 20));
     console.log(players);
     //drawBoard();//Der kaldes til funktionen 'drawBorad' som er længere nede
-    
+
     playerSelect.position(width / 2, 20);//Placering for valg af antal spillere knap
     turns = 0;//Der startes med ikke at være foretaget ture
     startGameBtn.size(100, 30);//Placering for start spil knap
@@ -52,23 +52,23 @@ function setup() {
     correctAnswBtn.hide();
     wrongAnswBtn.hide();
     answerBtn.hide();
-    startGameBtn.mousePressed(function(){//Når der trykkes på 'startgame' sker følgene:
-    playerSelect.hide();
-    startGameBtn.hide();//'startgame' skjules
-    diceBtn.show();//'dice' kommer frem på skærmen
-    drawBoard();
+    startGameBtn.mousePressed(function () {//Når der trykkes på 'startgame' sker følgene:
+        playerSelect.hide();
+        startGameBtn.hide();//'startgame' skjules
+        diceBtn.show();//'dice' kommer frem på skærmen
+        drawBoard();
 
-    for (let i = 0; i < playerSelect.value(); i++) {//Inden for i=0 til det valgte antal spillere sker følgende
-        //Opretter spiller efter nedenstående objekt
-        //For hver player skal en farve vælges og y- værdi for position
-        //For loopet kører player antal, dvs. det antal playere spilelren har valgt
-        //Farven vælges efter farve array
-        //Position for y-værdi vælges til sidst
-        players.push(new Player(playerColors[i], "player"+ (i+1), height / 3 * 2 + 40 * i));
-    }
-    console.log(players);
-    
-    drawBoard();
+        for (let i = 0; i < playerSelect.value(); i++) {//Inden for i=0 til det valgte antal spillere sker følgende
+            //Opretter spiller efter nedenstående objekt
+            //For hver player skal en farve vælges og y- værdi for position
+            //For loopet kører player antal, dvs. det antal playere spilelren har valgt
+            //Farven vælges efter farve array
+            //Position for y-værdi vælges til sidst
+            players.push(new Player(playerColors[i], "player" + (i + 1), height / 3 * 2 + 40 * i));
+        }
+        console.log(players);
+
+        drawBoard();
 
 
     })
@@ -90,7 +90,7 @@ function setup() {
         }
 
     });
-   
+
 
 }
 
@@ -110,7 +110,7 @@ function drawBoard() {//Funktion der tegner spillebrættet
     for (let i = 0; i < players.length; i++) {//Loppet kører det antal gange som der er players.
         //Length angiver længden af arrayet 'players'
         players[i].display();//Henviser til metoden display som er længere nede
-        console.log("players at drawboard: "+players);
+        console.log("players at drawboard: " + players);
     }
 }
 
