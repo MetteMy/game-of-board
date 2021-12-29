4fa14147e1dc5709c80e47f6c455576f947b00d3
MetteMy <mettemy@gabelgaard.org>
2021-12-29 13:26:19 +0100

    answer input and check

--- Board.js
+++ Board.js
@@ -14,50 +14,55 @@ let answerBtn;
 let turns;
 let playerSelect;
 let difficultySelect;
+let answerInput;
+let question;
 
 
 function setup() {
+    rectMode(CENTER); //Firkanter tegnes fra midten
+    textAlign(CENTER); //Tekst er centeret
     createCanvas(windowWidth, windowHeight);//Canvas tilpasses til computeren
     background("#1982C4");//blå
     strokeWeight(2);
 
-    rectMode(CENTER);//Firkanter tegnes fra midten
-    textAlign(CENTER);//Test er centeret
     textSize(20);
     gameOver = false;//Sætter variabel til false
-    //y = height / 2;
-    diceBtn = createButton('Dice').style('background-color', "white");;//Opretter knap: 'dice'
-    answerBtn = createButton('Answer').style('background-color', "white");;//Opretter knap: 'Answer'
-    correctAnswBtn = createButton('Correct answer').style('background-color', "#8AC926");;//Opretter knap: 'Correct answer'
-    wrongAnswBtn = createButton('Wrong answer').style('background-color', "#FF595E");;//Opretter knap: 'Wrong answer'
-    startGameBtn = createButton('startgame').style('background-color', "white");;//Opretter startknap: 'startgame'
-    playerSelect = createSelect().style('background-color', "white"); //Opretter selcect bar
+
+    diceBtn = createButton('Dice').style('background-color', "white");//Opretter knap: 'dice'
+    answerBtn = createButton('Answer').style('background-color', "white").position(width / 2, 160);//Opretter knap: 'Answer'
+    correctAnswBtn = createButton('Correct answer').style('background-color', "#8AC926").position(width / 2, 140);//Opretter knap: 'Correct answer'
+    wrongAnswBtn = createButton('Wrong answer').style('background-color', "#FF595E").position(width / 2, 140);//Opretter knap: 'Wrong answer'
+    startGameBtn = createButton('startgame').style('background-color', "white");//Opretter startknap: 'startgame'
+    playerSelect = createSelect().style('background-color', "white"); //Opretter select
+    answerInput = createInput().size(70).position(width / 2, 140);
     //Indsætter valgmuligheder for antal players
 
     playerSelect.option('2');
     playerSelect.option('3');
     playerSelect.option('4');
     playerSelect.option('5');
+    playerSelect.position(width / 2, 160);//Placering for valg af antal spillere knap
 
     //question difficulty select
-    difficultySelect = createSelect().style('background-color', "white"); 
+    difficultySelect = createSelect().style('background-color', "white");
     difficultySelect.option('easy');
     difficultySelect.option('medium');
     difficultySelect.option('hard');
-    difficultySelect.position(width / 2, 60);
+    difficultySelect.position(width / 2, 200);
 
-    //players.push(new Player("Pink", "pink", height / 2 + 20));
+    
     console.log(players);
-    //drawBoard();//Der kaldes til funktionen 'drawBorad' som er længere nede
+    
 
-    playerSelect.position(width / 2, 20);//Placering for valg af antal spillere knap
+    
     turns = 0;//Der startes med ikke at være foretaget ture
     startGameBtn.size(100, 30);//Placering for start spil knap
-    startGameBtn.position((width / 2) - 30, height / 2);
+    startGameBtn.position(width / 2, height / 2);
     diceBtn.hide();//'dice' kommer først frem når spillet starter
     correctAnswBtn.hide();
     wrongAnswBtn.hide();
     answerBtn.hide();
+    answerInput.hide();
     startGameBtn.mousePressed(function () {//Når der trykkes på 'startgame' sker følgene:
         playerSelect.hide();
         difficultySelect.hide();
@@ -74,17 +79,17 @@ function setup() {
             players.push(new Player(playerColors[i], "player" + (i + 1), height / 3 * 2 + 40 * i));
         }
 
-        if (difficultySelect.value() === 'easy'){
-        mathsQ = eMathsQ;
-        mathsA = eMathsA;
-        popcultureQ = ePopcultureQ;
-        popcultureA = ePopcultureA;
-        geographyQ = eGeographyQ;
-        geographyA = eGeographyA;
-        randomQ = eRandomQ;
-        randomA = eRandomA;
-        } 
-        if (difficultySelect.value() === 'medium'){
+        if (difficultySelect.value() === 'easy') {
+            mathsQ = eMathsQ;
+            mathsA = eMathsA;
+            popcultureQ = ePopcultureQ;
+            popcultureA = ePopcultureA;
+            geographyQ = eGeographyQ;
+            geographyA = eGeographyA;
+            randomQ = eRandomQ;
+            randomA = eRandomA;
+        }
+        if (difficultySelect.value() === 'medium') {
             mathsQ = mMathsQ;
             mathsA = mMathsA;
             popcultureQ = mPopcultureQ;
@@ -94,7 +99,7 @@ function setup() {
             randomQ = mRandomQ;
             randomA = mRandomA;
         }
-        if (difficultySelect.value() === 'hard'){
+        if (difficultySelect.value() === 'hard') {
             mathsQ = hMathsQ;
             mathsA = hMathsA;
             popcultureQ = hPopcultureQ;
@@ -103,7 +108,7 @@ function setup() {
             geographyA = hGeographyA;
             randomQ = hRandomQ;
             randomA = hRandomA;
-        }  
+        }
         console.log(players);
 
         drawBoard();
@@ -116,36 +121,48 @@ function setup() {
     //Dice
     diceBtn.position(10, 10);//Angiver positionen for terning
     diceBtn.size(70, 70);//Angiver terningens størrelse
-    diceBtn.mousePressed(function () {//Når der trykkes på terningen sker følgende:
-        //turns = turns + 1; // changes whoose turn it is
+    diceBtn.mousePressed(diceRoll);
+
+    document.addEventListener("keyup", function (event) {
+        //event.preventDefault();
+        if (keyCode == RIGHT_ARROW) { // piletast fordi et bogstav ville udløses når man skriver svar
+            diceRoll();
+            console.log("keycode is: " + keyCode);
+
+        }
+
+    });
+    function diceRoll() {
+
         console.log("turns: " + turns);
-        for (let i = 0; i < players.length; i++) {//Players.lenght angiver længden på arrayet
+        for (let i = 0; i < players.length; i++) {
             if (turns % players.length === i) {
                 players[i].die();
 
             }
 
         }
+    }
 
-    });
 
 
 }
+
 
 function drawBoard() {//Funktion der tegner spillebrættet
 
     for (let i = 0; i < 20; i++) {//Der tegnes 20 felter
         color = i % 4;//"Resten" når der divideres med 4. Angiver hvilket felt brikken rykker til
         push();//Det er kun det ene felt der bliver farvet
-        fill(colors[color]);//Arrayet hedder color. Derfor den første color
-        //Anden color angiver hvilket nummer i arrayet vi skal bruge (altså hvilken farve vi fylder med)
+        fill(colors[color]);//Arrayet hedder colors. Derfor colors
+        //color angiver hvilket nummer i arrayet vi skal bruge (altså hvilken farve vi fylder med)
         //Husk at color ovenover er defineret til at være lig med i%4
         rect(width / 20 * i + 32, height, 50, 450);//Tegner feltet. Felternes højde varieres efter antal spillere
         squareX.push(width / 20 * i + 32);//
         pop();//Ender push
     }
 
-    for (let i = 0; i < players.length; i++) {//Loppet kører det antal gange som der er players.
+    for (let i = 0; i < players.length; i++) {//Loopet kører det antal gange som der er players.
         //Length angiver længden af arrayet 'players'
         players[i].display();//Henviser til metoden display som er længere nede
         console.log("players at drawboard: " + players);
@@ -154,10 +171,6 @@ function drawBoard() {//Funktion der tegner spillebrættet
 
 
 
-
-function draw() {
-
-}
 
 class Player {
 
@@ -198,6 +211,8 @@ class Player {
         this.move();
         text(this.dieMoves, 90, 50);
 
+
+        // board is 20 tiles long, so when  we reach 20, the game is over 
         if (this.moves >= 20) {
             console.log("you won!!!");
             background("#1982C4");
@@ -206,21 +221,28 @@ class Player {
             answerBtn.hide();
             wrongAnswBtn.hide();
             correctAnswBtn.hide();
+            answerInput.hide();
             gameOver = true;
 
         }
     }
     giveQuestion() {
         if (gameOver === false) {
+            answerInput.value("");
             console.log(this.playercolor);
             push();
             fill(this.playercolor);
             rect(width / 2, 120, 1000, 250);
             pop();
+            push();
+            fill(colors[this.moves % 4]);
+            rect(width / 2, 120, 900, 230);
+            pop();
             if (this.moves % 4 === 0) {
                 // math 
                 category = mathsQ;
                 answer = mathsA;
+
             }
             if (this.moves % 4 === 1) {
                 // random
@@ -237,36 +259,61 @@ class Player {
                 category = popcultureQ;
                 answer = popcultureA;
             }
-            let question = parseInt(random(0, category.length));
+
+            question = parseInt(random(0, category.length));
             console.log("question number: " + question);
             text(category[question], width / 2, 120);
+            answerInput.show();
             answerBtn.show();
-            answerBtn.position(width / 2 - 30, 140);
-            answerBtn.mousePressed(function () {
-                text(answer[question], width / 2, 180);
-                category.splice(question, 1);// det her virker ikke 
+
+
+
+            answerBtn.mousePressed(checkAnswer);
+            document.addEventListener("keyup", function (event) {
+                //event.preventDefault();
+                if (keyCode == 13) {
+                    checkAnswer();
+                    console.log("keycode is: " + keyCode);
+
+                }
+
+            });
+
+
+            function checkAnswer() {
+                answerBtn.hide();
+                text(answer[question], width / 2, 200);
+
+                console.log("input :" + answerInput.value().toLowerCase());
+                console.log("answer at input check :" + answer[question].toLowerCase());
+
+
+                if (answer[question].toLowerCase().match(answerInput.value().toLowerCase())) {
+                    // if answer is correct
+                    text("Correct, you can try again!", width / 2, 230);
+                    diceBtn.show();
+                    console.log("the answer was correct");
+
+                } else {
+                    text("incorrect, next player", width / 2, 230);
+                    turns += 1;
+                    diceBtn.show();
+
+                }
+
+
+                /*category.splice(question, 1);// det her virker ikke 
                 answer.splice(question, 1);
                 answerBtn.hide();
                 console.log("category: " + category);
                 console.log("answer: " + answer);
-                correctAnswBtn.position(width / 2 + 30, 140);
-                wrongAnswBtn.position(width / 2 - 130, 140);
-                correctAnswBtn.show();
-                wrongAnswBtn.show();
+                
+                
+*/
+            }
 
-            });
-            wrongAnswBtn.mousePressed(function () {
-                turns += 1;
-                wrongAnswBtn.hide();
-                correctAnswBtn.hide();
-                diceBtn.show();
-            });
 
-            correctAnswBtn.mousePressed(function () {
-                wrongAnswBtn.hide();
-                correctAnswBtn.hide();
-                diceBtn.show();
-            });
+
 
 
         }
