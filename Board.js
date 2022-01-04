861ca8974f8036cdd46fbd803bef8f609581a76a
MetteMy <mettemy@gabelgaard.org>
2022-01-04 18:23:10 +0100

    i don't remember what i did, but it works

--- Board.js
+++ Board.js
@@ -288,7 +288,7 @@ class Player {
                 console.log("answer at input check :" + answer[question].toLowerCase());
 
 
-                if (answer[question].toLowerCase().match(answerInput.value().toLowerCase()) && answerInput.value().toLowerCase() != "") {
+                if (answer[question].toLowerCase().match(answerInput.value().toLowerCase()) && answerInput.value().toLowerCase() != "") { // det sidste for at man ikke får korrekt ved ikke at skrive noget
                     // if answer is correct
                     text("Correct, you can try again!", width / 2, 230);
                     diceBtn.show();
